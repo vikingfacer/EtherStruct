af426ab185ab5524c9e09aec1c0ce16804e3877a
JM <jacobmontpetit@gmail.com>
2025-07-28 22:45:45 -0400

    update build.zig rm comments and use add_module

--- build.zig
+++ build.zig
@@ -4,51 +4,32 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    const etherStruct_mod = b.addModule("EtherStruct", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "EtherStruct",
-        .root_module = lib_mod,
-    });
+    //const lib = b.addLibrary(.{
+    //    .linkage = .static,
+    //    .name = "EtherStruct",
+    //    .root_module = etherStruct_mod,
+    //});
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    //b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
+    const test_step = b.step("test", "Run unit tests");
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_module = etherStruct_mod,
     });
-    const coverage = b.option(bool, "test-coverage", "Generate test coverage") orelse false;
 
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    test_step.dependOn(&run_lib_unit_tests.step);
+
+    const coverage = b.option(bool, "test-coverage", "Generate test coverage") orelse false;
     if (coverage) {
         // with kcov
         lib_unit_tests.setExecCmd(&[_]?[]const u8{
@@ -57,20 +38,5 @@ pub fn build(b: *std.Build) void {
             "kcov-output", // output dir for kcov
             null, // to get zig to use the --test-cmd-bin flag
         });
-
-        // or with grindcov
-        //tests.setExecCmd(&[_]?[]const u8{
-        //    "grindcov",
-        //    //"--keep-out-file", // any grindcov flags can be specified here
-        //    "--",
-        //    null, // to get zig to use the --test-cmd-bin flag
-        //});
     }
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
